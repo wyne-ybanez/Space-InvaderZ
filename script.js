586ea5041c524e01f8231423660b3582d50ec2c0
wyne-ybanez <wyne_01@icloud.com>
2022-11-30 10:54:08 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d') // get canvas, set it to 2D
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+    constructor() {
+        
+    }
+}
+
