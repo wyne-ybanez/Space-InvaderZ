47b874b6b8928435d10cd9b4958fa414456d8b6c
wyne-ybanez <wyne_01@icloud.com>
2022-11-30 16:06:30 +0000

    Add player rotation

--- script.js
+++ script.js
@@ -5,66 +5,92 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 class Player {
-    /**
-     * ====== Constructor
-     * - starting position
-     * - velocity
-     * - body (image, width, height)
-     */
-    constructor() {
-        const image = new Image() // this comes from the JavaScript API
-        image.src = './img/spaceship.png' // get the image but takes time to load
-        const scale = 0.15;
-        const margin = 50
+  /**
+   * ====== Constructor
+   * - starting position
+   * - velocity
+   * - body (image, width, height)
+   */
+  constructor() {
+    const image = new Image(); // this comes from the JavaScript API
+    image.src = "./img/spaceship.png"; // get the image but takes time to load
+    const scale = 0.15;
+    const margin = 50;
 
-        this.velocity = {
-          x: 0,
-          y: 0,
-        };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
 
-        // Image has loaded hence, set attributes
-        image.onload = () => {
-            this.imageLoad = true
+    this.rotation = 0;
 
-            this.body = {
-              image: image,
-              width: image.width * scale,
-              height: image.height * scale,
-            }
+    // Image has loaded hence, set attributes
+    image.onload = () => {
+      this.imageLoad = true;
 
-            this.position = {
-              x: (canvas.width / 2) - (this.body.width / 2),
-              y: canvas.height - this.body.height - margin
-            };
-        }
+      this.body = {
+        image: image,
+        width: image.width * scale,
+        height: image.height * scale,
+      };
+
+      this.position = {
+        x: canvas.width / 2 - this.body.width / 2,
+        y: canvas.height - this.body.height - margin,
+      };
+    };
+  }
+
+  /**
+   * ====== Methods
+   */
+
+  /**
+   * Draw Player
+   * - draws player
+   * - sets player ship rotation
+   * - restore context after rotation
+   */
+  draw() {
+    // DEBUGGING - comment out if unused
+    // c.fillStyle = 'red'
+    // c.fillRect(this.position.x, this.position.y, this.body.width, this.body.height)
+
+    c.save();
+    // makes rotatation
+    c.translate(
+      player.position.x + player.body.width / 2,
+      player.position.y + player.body.height / 2
+    );
+    c.rotate(this.rotation);
+
+    // clean up after rotation
+    c.translate(
+      -player.position.x - player.body.width / 2,
+      -player.position.y - player.body.height / 2
+    );
+
+    c.drawImage(
+      this.body.image,
+      this.position.x,
+      this.position.y,
+      this.body.width,
+      this.body.height
+    );
+
+    c.restore();
+  }
+
+  /**
+   * Update Player
+   * - updates player position/changes
+   */
+  update() {
+    if (this.imageLoad) {
+      this.draw();
+      this.position.x += this.velocity.x;
     }
-
-    /**
-     * ====== Methods
-     * - draw Player
-     * - update player after movement
-     */
-    draw() {
-        // DEBUGGING - comment out if unused
-        // c.fillStyle = 'red'
-        // c.fillRect(this.position.x, this.position.y, this.body.width, this.body.height)
-        if (this.imageLoad) {
-            c.drawImage(
-                this.body.image,
-                this.position.x,
-                this.position.y,
-                this.body.width,
-                this.body.height
-            )
-        }
-    }
-
-    update() {
-        if (this.imageLoad) {
-            this.draw()
-            this.position.x += this.velocity.x
-        }
-    }
+  }
 }
 
 /**
@@ -108,14 +134,17 @@ function animate() {
     // left
     if (keys.a.pressed && (player.position.x >= 0)) {
       player.velocity.x = -7;
+      player.rotation = -.15
     }
     // right
     else if (keys.d.pressed && (player.position.x + player.body.width <= canvas.width)) {
       player.velocity.x = 7;
+      player.rotation = .15
     }
     // stop
     else {
       player.velocity.x = 0;
+      player.rotation = 0;
     }
 }
 
